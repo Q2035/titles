05d3a150ccc72c6b209767628bb9f70dab888bb2
Q2035 <2383862181@qq.com>
2020-04-24 16:46:48 +0800

    first commit

---
+++ src/main/java/com/example/title/TitleApplication.java
@@ -0,0 +1,13 @@
+package com.example.title;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/title/TitleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.title;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
